781f17e072933d25a8b92b922222a8657103a448
Zaeem Raza <116446838+Zaeem-Raza@users.noreply.github.com>
2023-09-12 02:15:02 -0700

    Add files via upload

---
+++ ZigZagConversion.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string convert(string s, int numRows)
+{
+    if (numRows <= 1 || s.size() < numRows)
+        return s;
+    bool f = false;
+    int curr = 0;
+    vector<string> strs(numRows);
+    for (int i = 0; i < s.size(); i++)
+    {
+        strs[curr] += s[i];
+        if (curr == numRows - 1 || curr == 0)
+        {
+            f = !f;
+        }
+        if (f)
+        {
+            curr++;
+        }
+        else
+        {
+            curr--;
+        }
+    }
+    string result = "";
+    for (std::string &i : strs)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int main()
+{
+    // string str = "Paypal\nis\nHiring";
+    // cout << str;
+    // cout << convert("BITMANIPULATIONPROBLEMS", 4) << endl;
+    string s1 = convert("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 20);
+    // cout << convert("PAYPALISHIRING", 3) << endl;
+    string s = "AMYBLNXZCKOWADJPVBEIQUCFHRTDGGSSEHFTRFIEUQGJDVPHKCWOILBXNJZMAYMKYNZZLLXOYAKMWPXBJNVQWCIOURVDHPTSUEGQSTFR";
+    cout << (s1 == s);
+    return 0;
+}
